ceeeb1384436ba95967f5321958db939a2d97f8f
Rosyad <rosyadblog@gmail.com>
2025-05-25 17:29:56 +0700

    fix(post): change redirect create post in unit test

--- tests/Feature/CreatePostTest.php
+++ tests/Feature/CreatePostTest.php
@@ -22,7 +22,7 @@ class CreatePostTest extends TestCase
             'user_id' => $user->id
         ]);
 
-        $response->assertRedirect('/posts');
+        $response->assertRedirect('/');
         $this->assertDatabaseHas('posts', [
             'title' => 'Example title',
             'content' => 'This is example body content.',
@@ -118,22 +118,6 @@ class CreatePostTest extends TestCase
     }
 
     #[Test]
-    public function published_at_can_be_null()
-    {
-        $user = User::factory()->create();
-        $this->actingAs($user);
-
-        $response = $this->post('/posts', [
-            'title' => 'Judul valid',
-            'content' => 'Konten valid',
-            'published_at' => null,
-        ]);
-
-        $response->assertSessionDoesntHaveErrors('published_at');
-        $response->assertRedirect('/posts');
-    }
-
-    #[Test]
     public function post_status_is_draft_if_is_draft_true()
     {
         $user = User::factory()->create();
@@ -194,6 +178,6 @@ class CreatePostTest extends TestCase
             'published_at' => '2025-05-25',
         ]);
         $response->assertSessionDoesntHaveErrors('published_at');
-        $response->assertRedirect('/posts');
+        $response->assertRedirect('/');
     }
 }
