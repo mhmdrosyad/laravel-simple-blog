af174e7b8fa104a5c50a26f4a70a6937ae876890
Rosyad <rosyadblog@gmail.com>
2025-05-25 15:22:24 +0700

    feat(post): create posts table

--- database/migrations/2025_05_25_081752_create_posts_table.php
+++ database/migrations/2025_05_25_081752_create_posts_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 60);
+            $table->text('content');
+            $table->date('published_at')->nullable();
+            $table->enum('status', ['draft', 'published'])->default('published');
+            $table->foreignId('user_id');
             $table->timestamps();
         });
     }
